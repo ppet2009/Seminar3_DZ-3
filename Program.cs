a71f1d83036a2b638baeefa53dc9ad2ec51cdf91
Peter <peter_8787@outlook.com>
2022-11-01 14:36:22 +0300

    решена Задача3

--- Program.cs
+++ Program.cs
@@ -37,6 +37,8 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 
 */
 
+/*
+
 double FindDistance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
@@ -55,7 +57,36 @@ Console.WriteLine("Input z2:");
 double z2 = Convert.ToDouble(Console.ReadLine());
 double findDistance = FindDistance(x1, y1, z1, x2, y2, z2);
 Console.WriteLine($"Distance ={findDistance} ");
+*/
 
 
+/* Задача 3
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+
+*/
+
+void  quad (int num)
+
+{
+    int step = 1;
+
+    while (step <= num)
+    {
+
+ Console.WriteLine($"куб числа { step } равен {step * step * step}");
+  
+  step ++;
+ } 
+
+}
+Console.WriteLine("input num");
+int num = Convert.ToInt32(Console.ReadLine());
+
+quad(num); 
 
 
